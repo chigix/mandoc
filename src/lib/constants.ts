701ee22f0a2b7f56308f17510b2bfd865327ee3e
Richard Lea <chigix@zoho.com>
2018-08-01 21:37:54 +0900

    Chore: main template file set to njk extension
    Signed-off-by: Richard Lea <chigix@zoho.com>

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -5,4 +5,4 @@ export const TPL_DEFAULT_DIR = path.join(TEMPLATE_DIR, 'default');
 export const TPL_KEIYAKU_DIR = path.join(TEMPLATE_DIR, 'keiyaku');
 export const TPL_SRC_PATH = 'source';
 export const TPL_LAYOUT_PATH = 'layout';
-export const TPL_HTML_PATH = 'layout/template.html';
+export const TPL_HTML_PATH = 'layout/template.njk';
