5d4c63624702cfb1a7c67d26b12843cc705a3690
Richard Lea <chigix@zoho.com>
2018-08-02 11:18:22 +0900

    Test: markdown rendering stream and template rendering stream
    Signed-off-by: Richard Lea <chigix@zoho.com>

--- test/html.render.test.ts
+++ test/html.render.test.ts
@@ -1,3 +1,43 @@
-test('basic', () => {
-    expect(2).toBe(2);
+import * as fs from 'fs';
+import * as path from 'path';
+import * as through from 'through2';
+import { renderHtml, renderMarkdown } from '../src/scripts/html.render';
+import { Doc } from '../src/types';
+import { TEMPLATE_DIR } from './constants';
+
+function markdownStream(file: string) {
+  return fs.createReadStream(file)
+    .pipe(renderMarkdown({
+      baseDir: path.dirname(file),
+    }));
+}
+
+test('Basic RenderMarkdown', () => {
+  markdownStream(path.join(__dirname, './fixtures/ipsum.md'))
+    .pipe(through.obj((report: Doc, enc, cb) => {
+      expect(report.body).toBe(
+        fs.readFileSync(path.join(__dirname, './fixtures/ipsum.html'))
+          .toString('utf8'));
+    }));
+  markdownStream(path.join(__dirname, './fixtures/keiyaku-test.md'))
+    .pipe(through.obj((report: Doc, enc, cb) => {
+      expect(report.body).toBe(
+        fs.readFileSync(path.join(__dirname, './fixtures/keiyaku-test.html'))
+          .toString('utf8'));
+      cb();
+    }));
+});
+
+test('Basic Template Rendering', done => {
+  markdownStream(path.join(__dirname, './fixtures/ipsum.md'))
+    .pipe(renderHtml({
+      tplDir: path.join(TEMPLATE_DIR, 'default'),
+    })).pipe(through((chunk: Buffer, enc, cb) => {
+      expect(enc).toBe('buffer');
+      expect(chunk).toEqual(
+        fs.readFileSync(path.join(__dirname, './fixtures/ipsum-in-template.html')),
+      );
+      cb();
+      // fs.writeFileSync(path.join(__dirname, './fixtures/ipsum-in-template.html'), chunk);
+    }));
 });
