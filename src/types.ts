49f0de9ca4c81db4f6c90fef5ad6892438a79453
Richard Lea <chigix@zoho.com>
2018-08-03 03:29:54 +0900

    Feat: define type named TemplateConfiguration
    Signed-off-by: Richard Lea <chigix@zoho.com>

--- src/types.ts
+++ src/types.ts
@@ -1,3 +1,4 @@
+import * as stream from 'stream';
 import { DEFAULT_CONVERTER_OPTS } from './commanders';
 
 export interface Doc {
@@ -12,4 +13,35 @@ export interface Doc {
   bibliography?: {}[];
 }
 
-export const ConvertCommandOptions = typeof DEFAULT_CONVERTER_OPTS;
+export type ConvertCommandOptions = typeof DEFAULT_CONVERTER_OPTS;
+
+export interface TemplateConfiguration {
+  /**
+   * The primary entry point to the template usage.
+   * `layout/template.njk` is default.
+   */
+  main: string;
+
+  /**
+   * The primary directory where styles files are stored.
+   * `css`, `less` files are all supported.
+   *
+   * `source/` directory is default.
+   */
+  cssBaseDir?: string;
+
+  /**
+   * The primary directory where javascript script files are stored.
+   *
+   * `source/` directory is default.
+   */
+  jsBaseDir?: string;
+  renderer?: (register:
+    (name: string, output: string, processStream: stream.Transform) => void,
+    // tplCtx: Context // Plan in the future for support getting mandoc context
+    //                    in processStream.
+  ) => never;
+  helper?: (register:
+    (name: string, fn: (...args: (string | Object)[]) => string) => never,
+  ) => never;
+}
