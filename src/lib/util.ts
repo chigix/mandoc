a046ffa55266665088d48bf811ca15079acd714e
Richard Lea <chigix@zoho.com>
2018-08-03 21:45:51 +0900

    Feat: util functions on json check and linter
    Signed-off-by: Richard Lea <chigix@zoho.com>

--- src/lib/util.ts
+++ src/lib/util.ts
@@ -2,6 +2,7 @@ import * as fs from 'fs';
 import * as path from 'path';
 import { isString } from 'util';
 const slash = require('slash');
+const jsonlint = require('jsonlint');
 
 export function urlFor(p: string, ctx: {
   baseDir: fs.PathLike,
@@ -23,4 +24,21 @@ export function urlFor(p: string, ctx: {
   return slash(path.join('/', path.relative(
     root_dir_path, path.join(base_dir_path, p),
   )));
+}
+
+export function isJSONString(text: string) {
+  return text.startsWith('{') &&
+    text.endsWith('}');
+}
+
+export function isFile(filePath: fs.PathLike) {
+  return fs.existsSync(filePath) && !fs.lstatSync(filePath).isDirectory();
+}
+
+export function jsonlintErrCatch(input: string) {
+  try {
+    jsonlint.parse(input);
+  } catch (error) {
+    return error.stack;
+  }
 }
