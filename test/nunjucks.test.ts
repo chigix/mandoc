4f458f92cbf163cfc5c868950b811a5e8ef14212
Richard Lea <chigix@zoho.com>
2018-09-05 02:16:38 +0900

    Test: template render case to test basic import
    Signed-off-by: Richard Lea <chigix@zoho.com>

--- test/nunjucks.test.ts
+++ test/nunjucks.test.ts
@@ -4,11 +4,13 @@ import * as path from 'path';
 import * as CONST from './constants';
 const NjkLoaders = require('../tools/njk-loader');
 
+function getNJKEnv(tplDir: string | string[]) {
+  return new nunjucks.Environment(
+    new NjkLoaders.TemplateLoader(tplDir));
+}
+
 test('TemplateLoader Basic Usage', done => {
-  const njk_env = new nunjucks.Environment(
-    new NjkLoaders.TemplateLoader(path.join(CONST.TEST_TEMPLATE_DIR, './basic-render-case')),
-    // new nunjucks.FileSystemLoader(path.join(CONST.TEST_TEMPLATE_DIR, './basic-render-case')),
-  );
+  const njk_env = getNJKEnv(path.join(CONST.TEST_TEMPLATE_DIR, './basic-render-case'));
   njk_env.render('layout/template.njk', {
     title: '逃げる',
     body: '逃げちゃだめだ。逃げちゃだめだ。逃げちゃだめだ。',
@@ -21,6 +23,25 @@ test('TemplateLoader Basic Usage', done => {
       fs.readFileSync(
         path.resolve(CONST.RENDER_CASE_DIR, './basic-render-nigeru.html'),
         'utf8'));
+
+    return done();
+  });
+});
+
+test('Basic External Source Import Testing', done => {
+  const njk_env = getNJKEnv([
+    path.join(CONST.TEST_TEMPLATE_DIR, './import-render-case'),
+    path.join(CONST.TEST_TEMPLATE_DIR, './import-render-case/source'),
+  ]);
+  njk_env.render('layout/template.njk', {
+    title: '逃げる',
+    body: '逃げちゃだめだ。逃げちゃだめだ。逃げちゃだめだ。',
+  }, (err, result) => {
+    expect(err).toBeNull();
+    expect(result).toEqual(
+      fs.readFileSync(
+        path.resolve(CONST.RENDER_CASE_DIR, './import-render-nigeru.html')
+        , 'utf8'));
 
     return done();
   });
