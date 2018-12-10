31dabc7b2fe69901374f40abafed47ddacd789f9
Richard Lea <chigix@zoho.com>
2018-12-11 00:24:59 +0900

    fix(fixtures): add path helper and read helper for fixtures

--- fixtures/index.ts
+++ fixtures/index.ts
@@ -1,5 +1,17 @@
+import * as fs from 'fs-extra';
 import * as path from 'path';
+const slash = require('slash');
 
-export const FIXTURES_DIR = __dirname;
-export const TPL_TEST_FIXTURE = path.join(FIXTURES_DIR, './tpl-test');
-export const RENDER_CASE_FIXTURE = path.join(FIXTURES_DIR, './tpl-render-result');
+type Path = string;
+
+export const FIXTURES_DIR = slash(__dirname);
+export const TPL_TEST_FIXTURE = slash(path.join(FIXTURES_DIR, './tpl-test'));
+export const RENDER_CASE_FIXTURE = slash(path.join(FIXTURES_DIR, './tpl-render-result'));
+
+export function fixture(name: Path) {
+  return slash(path.resolve(FIXTURES_DIR, name));
+}
+
+export function readFixture(name: Path) {
+  return fs.readFileSync(path.resolve(FIXTURES_DIR, name), 'utf8');
+}
