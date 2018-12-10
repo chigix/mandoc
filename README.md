87bdfe7a98b3894789ac3a505427eec344512645
Richard Lea <chigix@zoho.com>
2018-12-11 00:27:49 +0900

    build(whole-repo): update publish script to include front-matter package

--- README.md
+++ README.md
@@ -23,19 +23,6 @@ This project is inspired by
 
 ## To Try
 
-* tsconfig file series practice
-  It seems that `esm5` and `esm2015` compiling result from typescript source
-  code could be directly used for the keys, [`module`](https://github.com/rollup/rollup/wiki/pkg.module)
-  and `es2015`,  in `package.json` file.
-
-  This practice could also be totally referenced in `rxjs` official repository,
-  where different tsconfig files are prepared for compiling target as `es5` and
-  `es2015`.
-
-  This attempt sounds that a little time would be cost without any apparent technical
-  barrier that need to spend time for inspecting and learning.
-* RXJS commit message guideline
-  https://github.com/ReactiveX/rxjs/blob/master/CONTRIBUTING.md#commit-message-guidelines
 * Default Slide Template Support Plan
 
 ## Changelog
@@ -43,6 +30,7 @@ This project is inspired by
 Learn about the latest improvements:
 
 * [ManDoc's Changelog](./packages/mandoc/CHANGELOG.md)
+* [Remarkable Front-Matter Changelog](./packages/remarkable-front-matter/CHANGELOG.md)
 * [Paper Template Changelog](./packages/tpl-paper/CHANGELOG.md)
 
 ## License
--- publish.sh
+++ publish.sh
@@ -1,7 +1,17 @@
 #!/bin/bash
 
 REPO_ROOT=$(cd `dirname $0` && pwd)
+
+case $(uname -s) in
+    # Linux*)     machine=Linux;;
+    # Darwin*)    machine=Mac;;
+    # CYGWIN*)    machine=Cygwin;;
+    MINGW*)     REPO_ROOT=$(echo $REPO_ROOT | sed 's/^\///' | sed 's/\//\//g' | sed 's/^./\0:/');;
+    # *)          machine="UNKNOWN:${unameOut}"
+esac
+
 PKG_MANDOC_ROOT="$REPO_ROOT/packages/mandoc"
+PKG_FRONT_MATTER_ROOT="$REPO_ROOT/packages/remarkable-front-matter"
 PKG_TPL_PAPER_ROOT="$REPO_ROOT/packages/tpl-paper"
 
 function publishPackage {
@@ -9,12 +19,43 @@ function publishPackage {
   package_name=$(node -p "require('$package_path/package.json').name")
   package_version=$(node -p "require('$package_path/package.json').version")
   read -p "Building $package_name@$package_version. [Enter] to continue"
-  npx lerna run --scope=$package_name build
+  npx lerna run --stream --scope=$package_name build
   read -p "Ready to publish $package_name@$package_version. [Enter] to continue"
   cd $package_path/$(dirname $(node -p "require('$package_path/package.json').main"))
   npm publish
   cd $REPO_ROOT
 }
+
+if [ \
+    $(npm view remarkable-front-matter version) \
+    = $(node -p "require('$PKG_FRONT_MATTER_ROOT/package.json').version") ]; then
+  echo "remarkable-front-matter: up to date"
+else
+  echo "Publish Package: remarkable-front-matter?"
+  select yn in "Yes" "No"; do
+    case $yn in
+      Yes) publishPackage $PKG_FRONT_MATTER_ROOT; break;;
+      No) echo "Skipped publishing `remarkable-front-matter`"; break;;
+    esac
+  done
+fi
+
+cd $REPO_ROOT
+if [ \
+    $(npm view mandoc-template-paper version) \
+    = $(node -p "require('$PKG_TPL_PAPER_ROOT/package.json').version") ]; then
+  echo "mandoc-template-paper: up to date"
+else
+  echo "Publish Package: mandoc-template-paper?"
+  select yn in "Yes" "No"; do
+    case $yn in
+      Yes) publishPackage $PKG_TPL_PAPER_ROOT; break;;
+      No) echo "Skipped publishing `mandoc-template-paper`"; break;;
+    esac
+  done
+fi
+
+cd $REPO_ROOT
 
 if [ \
     $(npm view mandoc version) \
@@ -36,19 +77,3 @@ else
     esac
   done
 fi
-cd $REPO_ROOT
-if [ \
-    $(npm view mandoc-template-paper version) \
-    = $(node -p "require('$PKG_TPL_PAPER_ROOT/package.json').version") ]; then
-  echo "mandoc-template-paper: up to date"
-else
-  echo "Publish Package: mandoc-template-paper?"
-  select yn in "Yes" "No"; do
-    case $yn in
-      Yes) publishPackage $PKG_TPL_PAPER_ROOT; break;;
-      No) echo "Skipped publishing `mandoc-template-paper`"; break;;
-    esac
-  done
-fi
-
-cd $REPO_ROOT
